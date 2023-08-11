6617da7492668e9fb93a001f09be10600f8b3e45
Breno <105722593+BrenoSilva20@users.noreply.github.com>
2023-08-11 17:45:06 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    question: 'Se você me conhece bem qual o anime que eu sou mais fã?',
+    answers: [
+      { option: 'Cavaleiros do Zodiacos', correct: true },
+      { option: 'Naruto', correct: false },
+      { option: 'Tokyo Revengers', correct: false }
+    ]
+  },
+  {
+    question: 'Qual foi o Primeior anime que assistimos pela primeira vez?',
+    answers: [
+      { option: 'Inuyasha', correct: false },
+      { option: 'Classroom of the elite', correct: true },
+      { option: 'Akame ga kill', correct: false }
+    ]
+  },
+  {
+    question:
+      'Qual foi o assunto que fez nossa amizade se tornar cada vez mais especial para nós?',
+    answers: [
+      { option: 'Nossas conversas sobre vaviras coisas', correct: false },
+      { option: 'Nosso interesse por programação', correct: false },
+      { option: 'Do anime/manga do noia que volta no tempo?', correct: true }
+    ]
+  },
+  {
+    question:
+      'Agora a mais importante, quando não conseguimos assistir nossos animes no dia e no horario combinado?',
+    answers: [
+      { option: 'Problemas com a internete', correct: false },
+      { option: 'Imprevistos ou horarios que não se batem', correct: false },
+      { option: 'Mikey!!!', correct: true }
+    ]
+  }
+]
